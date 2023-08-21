a16f77f072ecee948e16775d88783f390844acdc
Mutavore <142659203+Mutavore@users.noreply.github.com>
2023-08-21 17:05:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQT5T6VrHJoaChZBjR662E5PgbJE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'mwq8p-Y5BEI1urTJIYorCqycIc5ZUEL4UnNB8yIeh6w',
+      useTemplateId: 'w04pvcBjTL0UgQjJ3IJk-2QRkMiTIwgX3tgHSzPv3Ns',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -47,7 +47,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'w04pvcBjTL0UgQjJ3IJk-2QRkMiTIwgX3tgHSzPv3Ns',
+  CALLBACK_TEMPLATE_ID: 'mwq8p-Y5BEI1urTJIYorCqycIc5ZUEL4UnNB8yIeh6w',
 
   CALLBACK_USERS: [
     {
