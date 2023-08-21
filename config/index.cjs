71a1105dcc2ad0ce777fb61eb188f8d112724aa4
Mutavore <142659203+Mutavore@users.noreply.github.com>
2023-08-21 19:52:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -92,22 +92,34 @@ const USER_CONFIG = {
       useTemplateId: 'w04pvcBjTL0UgQjJ3IJk-2QRkMiTIwgX3tgHSzPv3Ns',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-09',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '03-24',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-07-10' },
-        
-      ],
+      
+      FESTIVALS: [
+    // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    {
+      type: '*生日', name: '宝贝', year: '2001', date: '09-09', isShowAge: true,
     },
+  
+    // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+    {
+      type: '*生日', name: '田昊', year: '1996', date: '09-14', isShowAge: true,
+    },
+    {
+      type: '节日', name: '相识纪念日', year: '2023', date: '03-24',
+    }
+    // 可以不断按格式往下增加
+    // ...
   ],
+
+      FESTIVALS_LIMIT: 2,
+      
+      CUSTOMIZED_DATE_LIST: [
+    // 在一起的日子
+    { keyword: 'love_day', date: '2023-7-10' },
+    // 认识的日子
+    { keyword: 'marry_day', date: '2023-03-24' },
+    // ...
+  ],
+  
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
