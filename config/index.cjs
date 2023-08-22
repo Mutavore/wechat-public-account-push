365cfaac3dd21229cf9af63083ef25f75b7cf2a6
Mutavore <142659203+Mutavore@users.noreply.github.com>
2023-08-22 16:39:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -83,7 +83,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQT5T6VrHJoaChZBjR662E5PgbJE',
+      id: 'oQT5T6X2SqEv8D7E55zsRyhwRhfE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'IxlayogoDPMlo7Zr7S2xGhHbsMlt4hFUyHxzQHgK1Gw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
