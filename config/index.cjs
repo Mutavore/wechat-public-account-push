84d18c244d24fe3b4ddd2e8abd2afac90ada34ff
Mutavore <142659203+Mutavore@users.noreply.github.com>
2023-08-22 16:09:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -85,7 +85,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQT5T6VrHJoaChZBjR662E5PgbJE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'nXRn4sCekvZS9YAUx4T1BeueP22U0lMQdaRIM-Ie0vI',
+      useTemplateId: 'Gsf7XCvU_IzeJnSujN6b3OVaBwQj1qbjodCQ8Eu5Vcs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-09',
       festivals: [
